d37161ac7da1a2844f5c35d2625e399b5d1f0cae
Cluelesshint <connorwaggoner98@gmai.com>
2022-03-18 16:57:41 -0400

    add gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "",
+    publicPath: "/webpack-demo/",
   },
   mode: "production",
   module: {
